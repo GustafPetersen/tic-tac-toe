215da22eaef137e34f4a3ff093b48d67a0f16b00
Gustaf Petersen <lutters@Air-MCL.local>
2022-09-05 08:23:09 +0200

    adding initial css, html, and js files

---
+++ main.js
@@ -0,0 +1,18 @@
+const gameBoard = () => {
+    const selectSquare = () => {
+        // selects the square that the player has chosen
+        return;
+    }
+    
+    const squareArray = document.body.querySelectorAll(".player-square")
+    squareArray.forEach(element => {
+        gameArray.push(element, null)
+    });
+    let gameArray = [];
+    return gameArray;
+
+}
+
+const playerSelect = (playerName, playerSymbol) => {gg
+        return {playerName, playerSymbol};
+}
